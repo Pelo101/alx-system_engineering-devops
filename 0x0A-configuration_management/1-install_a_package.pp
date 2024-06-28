8a0c99440e6bc6fffa155d1fe4071a4553cb2477
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-06-28 08:16:13 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,6 @@
 
 exec {'install_flask and _werkzeug':
     command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.3',
-    path => ['/usr/bin', '/usr/local/bin'],
+    path    => ['/usr/bin', '/usr/local/bin'],
 
 }
