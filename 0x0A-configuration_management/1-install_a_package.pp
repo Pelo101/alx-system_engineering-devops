9fcc83a7a95f4856d1457d2113f7be5f4ed9ebf3
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-06-28 08:12:03 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Using Puppet, install flask from pip3.
 
-exec {'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
+exec {'install_flask and _werkzeug':
+    command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.3',
     path => ['/usr/bin', '/usr/local/bin'],
 
 }
