1aca61d7affd75c660cb6f42231a71563ade45d2
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-08-24 10:34:05 +0000

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,5 @@
+# Update limit request limint to prevent failures
+
 exec { 'update_limit_and_restart' :
   command => "sed -i 's/15/15000/' /etc/default/nginx && service nginx restart",
   path    => ['/bin', '/usr/bin'],
